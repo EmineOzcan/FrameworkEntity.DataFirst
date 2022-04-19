842cdbb0e30147794c9d5b6dd0cb6f92225e8372
emine özcan <ozcan-emine@hotmail.com>
2022-04-20 01:15:08 +0300

    Add project files.

---
+++ FrameworkEntity.DataFirst/DAL/DbContextInitializer.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrameworkEntity.DataFirst.DAL
+{
+   
+    public class DbContextInitializer
+    {
+        public static IConfigurationRoot Configuration;
+        public static DbContextOptionsBuilder<AppDbContext> OptionsBuilder;
+
+        public static void Build()
+        {
+
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", optional: true,
+                reloadOnChange: true);
+
+            var configuration = builder.Build();
+            OptionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            OptionsBuilder.UseSqlServer(configuration.GetConnectionString("SqlCon"));
+         }
+
+    }
+}
---
+++ FrameworkEntity.DataFirst/Program.cs
@@ -0,0 +1,17 @@
+﻿using FrameworkEntity.DataFirst.DAL;
+using System.Linq;
+using System;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+
+DbContextInitializer.Build();
+
+using (var _context = new AppDbContext(DbContextInitializer.OptionsBuilder.Options))
+{
+    var products = await _context.Products.ToListAsync();
+
+    products.ForEach(p =>
+        Console.WriteLine($" p = {p.Name} : {p.Price}")
+    );
+}
